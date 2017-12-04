86d2c6133640e5213a3b66f2b3c00a739f37f853
aim100k <34214145+aim100k@users.noreply.github.com>
2017-12-04 05:53:46 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
-sudo dpkg -i bin/nodejs_9.1.0-1nodesource1_amd64.deb
-sudo dpkg -i bin/yarn_1.3.2-1_all.deb
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update
+sudo apt-get install -y nodejs yarn
 
 mkdir -p ~/code/bins
-tar xzf bin/bower.tar.gz -C ~/code/bins
+tar xf bin/bower.tar.gz -C ~/code/bins
 export PATH=/home/ubuntu-gnome/code/bins/node_modules/.bin:$PATH
-
